ed808d93d392ef5f780a796dbc9f0d33dd240a0e
Rachel <drachel276@icloud.com>
2024-09-15 17:32:07 -0400

    Initial commit

---
+++ Spotify/Spotify/SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyApp.swift
+//  Spotify
+//
+//  Created by Rachel Dhinakar on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Spotify/Spotify/SpotifyView.swift
@@ -0,0 +1,18 @@
+//
+//  SpotifyView.swift
+//  Spotify
+//
+//  Created by Rachel Dhinakar on 10/7/24.
+//
+
+import SwiftUI
+
+struct SpotifyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpotifyView()
+}
